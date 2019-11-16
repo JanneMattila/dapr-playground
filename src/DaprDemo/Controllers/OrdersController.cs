9870b2f51be3b93d4d5980bc065bd6bbc5209391
Janne Mattila <janne.mattila@hotmail.fi>
2019-11-16 19:28:27 +0200

    Add simple orders controller

---
+++ src/DaprDemo/Controllers/OrdersController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DaprDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DaprDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        // GET: api/Orders
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Orders/5
+        [HttpGet("{id}", Name = "Get")]
+        public Order Get(string id)
+        {
+            return new Order()
+            {
+                ID = id
+            };
+        }
+
+        // POST: api/Orders
+        [HttpPost]
+        public void Post([FromBody] Order value)
+        {
+        }
+
+        // PUT: api/Orders/5
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody] Order value)
+        {
+        }
+
+        // DELETE: api/Orders/5
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+        }
+    }
+}
---
+++ src/DaprDemo/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DaprDemo.Models
+{
+    public class Order
+    {
+        [JsonPropertyName("id")]
+        public string ID { get; set; }
+    }
+}
